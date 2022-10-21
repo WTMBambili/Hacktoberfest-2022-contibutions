c787de3cb0e35207bf4d98d9f2622fe766cdb7e4
Mofiro Jean <mofirojean@gmail.com>
2022-10-21 15:59:58 +0100

    feat: added a helloworld program in Python

---
+++ Task4/hello_world_mofirojean.py
@@ -0,0 +1,10 @@
+# LANGUAGE: Javascript
+# ENV: Node.js
+# AUTHOR: Stephanie Nkwatoh
+# GITHUB: https://github.com/mofirojean
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
